17ae293947a6e111a9d2db5a19aa208f3523cd82
mhidalgo <mhidalgo@cipf.es>
2017-12-21 15:47:28 +0100

    Moving load.R to hipathia package, adding pseudo.mgi data documentation

--- R/data.R
+++ R/data.R
@@ -263,3 +263,93 @@
 #'
 #' @usage data(xref)
 "xref"
+
+
+#' Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @format Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @return Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @usage data(pmgi_hsa_uniprot)
+"pmgi_hsa_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, HSA
+#'
+#' @format Object of pathways information grouped by GO functions, HSA
+#'
+#' @return Object of pathways information grouped by GO functions, HSA
+#'
+#' @usage data(pmgi_hsa_GO)
+"pmgi_hsa_GO"
+
+
+#' Object of pathways information grouped by genes, HSA
+#'
+#' @format Object of pathways information grouped by genes, HSA
+#'
+#' @return Object of pathways information grouped by genes, HSA
+#'
+#' @usage data(pmgi_hsa_genes)
+"pmgi_hsa_genes"
+
+
+#' Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @format Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @return Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @usage data(pmgi_mmu_uniprot)
+"pmgi_mmu_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, MMU
+#'
+#' @format Object of pathways information grouped by GO functions, MMU
+#'
+#' @return Object of pathways information grouped by GO functions, MMU
+#'
+#' @usage data(pmgi_mmu_GO)
+"pmgi_mmu_GO"
+
+
+#' Object of pathways information grouped by genes, MMU
+#'
+#' @format Object of pathways information grouped by genes, MMU
+#'
+#' @return Object of pathways information grouped by genes, MMU
+#'
+#' @usage data(pmgi_mmu_genes)
+"pmgi_mmu_genes"
+
+
+#' Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @format Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @return Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @usage data(pmgi_rno_uniprot)
+"pmgi_rno_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, RNO
+#'
+#' @format Object of pathways information grouped by GO functions, RNO
+#'
+#' @return Object of pathways information grouped by GO functions, RNO
+#'
+#' @usage data(pmgi_rno_GO)
+"pmgi_rno_GO"
+
+
+#' Object of pathways information grouped by genes, RNO
+#'
+#' @format Object of pathways information grouped by genes, RNO
+#'
+#' @return Object of pathways information grouped by genes, RNO
+#'
+#' @usage data(pmgi_rno_genes)
+"pmgi_rno_genes"
