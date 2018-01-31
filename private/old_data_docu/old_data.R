cff19973192ea5f39ff6eda9dd8c329202a0aee9
Marta R. Hidalgo <marta.hidalgo@outlook.es>
2018-01-31 16:28:19 +0100

    Changing annotation files to inst/extdata folder

---
+++ private/old_data_docu/old_data.R
@@ -0,0 +1,198 @@
+#' GO annotations to pathways object
+#'
+#' @format GO annotations to pathways object
+#'
+#' @return GO annotations to pathways object
+#'
+#' @usage data(annofuns_GO)
+"annofuns_GO"
+
+
+#' Uniprot annotations to pathways object
+#'
+#' @format Uniprot annotations to pathways object
+#'
+#' @return Uniprot annotations to pathways object
+#'
+#' @usage data(annofuns_uniprot)
+"annofuns_uniprot"
+
+
+#' Entrez - HGNC annotations object
+#'
+#' @format Entrez - HGNC annotations object
+#'
+#' @return Entrez - HGNC annotations object
+#'
+#' @usage data(entrez_hgnc)
+"entrez_hgnc"
+
+
+#' GO annotations object
+#'
+#' @format GO annotations object
+#'
+#' @return GO annotations object
+#'
+#' @usage data(go_bp_annots)
+"go_bp_annots"
+
+
+#' GO graph information
+#'
+#' @format GO graph information
+#'
+#' @return GO graph information
+#'
+#' @usage data(go_bp_frame)
+"go_bp_frame"
+
+
+#' GO graph
+#'
+#' @format GO graph
+#'
+#' @return GO graph
+#'
+#' @usage data(go_bp_net)
+"go_bp_net"
+
+
+#' Uniprot annotations object
+#'
+#' @format Uniprot annotations object
+#'
+#' @return Uniprot annotations object
+#'
+#' @usage data(uni_bp_annots)
+"uni_bp_annots"
+
+
+#' Object of pathways information, HSA
+#'
+#' @format Object of pathways information, HSA
+#'
+#' @return Object of pathways information, HSA
+#'
+#' @usage data(meta_graph_info_hsa)
+"meta_graph_info_hsa"
+
+
+#' Object of pathways information, MMU
+#'
+#' @format Object of pathways information, MMU
+#'
+#' @return Object of pathways information, MMU
+#'
+#' @usage data(meta_graph_info_mmu)
+"meta_graph_info_mmu"
+
+
+#' Object of pathways information, RNO
+#'
+#' @format Object of pathways information, RNO
+#'
+#' @return Object of pathways information, RNO
+#'
+#' @usage data(meta_graph_info_rno)
+"meta_graph_info_rno"
+
+
+#' Translation object
+#'
+#' @format Translation object
+#'
+#' @return Translation object
+#'
+#' @usage data(xref)
+"xref"
+
+
+#' Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @format Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @return Object of pathways information grouped by Uniprot functions, HSA
+#'
+#' @usage data(pmgi_hsa_uniprot)
+"pmgi_hsa_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, HSA
+#'
+#' @format Object of pathways information grouped by GO functions, HSA
+#'
+#' @return Object of pathways information grouped by GO functions, HSA
+#'
+#' @usage data(pmgi_hsa_GO)
+"pmgi_hsa_GO"
+
+
+#' Object of pathways information grouped by genes, HSA
+#'
+#' @format Object of pathways information grouped by genes, HSA
+#'
+#' @return Object of pathways information grouped by genes, HSA
+#'
+#' @usage data(pmgi_hsa_genes)
+"pmgi_hsa_genes"
+
+
+#' Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @format Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @return Object of pathways information grouped by Uniprot functions, MMU
+#'
+#' @usage data(pmgi_mmu_uniprot)
+"pmgi_mmu_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, MMU
+#'
+#' @format Object of pathways information grouped by GO functions, MMU
+#'
+#' @return Object of pathways information grouped by GO functions, MMU
+#'
+#' @usage data(pmgi_mmu_GO)
+"pmgi_mmu_GO"
+
+
+#' Object of pathways information grouped by genes, MMU
+#'
+#' @format Object of pathways information grouped by genes, MMU
+#'
+#' @return Object of pathways information grouped by genes, MMU
+#'
+#' @usage data(pmgi_mmu_genes)
+"pmgi_mmu_genes"
+
+
+#' Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @format Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @return Object of pathways information grouped by Uniprot functions, RNO
+#'
+#' @usage data(pmgi_rno_uniprot)
+"pmgi_rno_uniprot"
+
+
+#' Object of pathways information grouped by GO functions, RNO
+#'
+#' @format Object of pathways information grouped by GO functions, RNO
+#'
+#' @return Object of pathways information grouped by GO functions, RNO
+#'
+#' @usage data(pmgi_rno_GO)
+"pmgi_rno_GO"
+
+
+#' Object of pathways information grouped by genes, RNO
+#'
+#' @format Object of pathways information grouped by genes, RNO
+#'
+#' @return Object of pathways information grouped by genes, RNO
+#'
+#' @usage data(pmgi_rno_genes)
+"pmgi_rno_genes"
--- private/pack_data_by_species.R
+++ private/pack_data_by_species.R
@@ -5,172 +5,185 @@
 
 
 species <- c("hsa", "mmu", "rno")
+dbs <- c("GO", "uniprot")
+edpath <- "inst/extdata/"
 
 # XREF
 #------------------------
-xref_all <- NULL
 for(spe in species){
     xref_folder <- paste0("private/xref/", spe, "/")
     xref_file <- paste0(xref_folder, "xref_", spe, ".RData")
     xr <- load(xref_file)
     xref <- get(xr)
-    xref_all[[spe]] <- xref
+    save(xref, file=paste0(edpath, "xref_", spe, ".rda"), compress = "bzip2")
 }
-xref <- xref_all
-devtools::use_data(xref, pkg = ".")
 
 
 # ANNOTATIONS
 #------------------------
 load("private/annotations/go_bp_frame.RData")
-devtools::use_data(go_bp_frame, pkg = ".")
+save(go_bp_frame, file = paste0(edpath, "go_bp_frame.rda"), compress = "bzip2")
 load("private/annotations/go_bp_net.RData")
-devtools::use_data(go_bp_net, pkg = ".")
+save(go_bp_net, file = paste0(edpath, "go_bp_net.rda"), compress = "bzip2")
 
 # GO_bp_annots
-go_bp_annots <- NULL
 for(spe in species){
     gba_folder <- paste0("private/annotations/", spe, "/")
     gba_file <- paste0(gba_folder, "/go_bp_", spe, ".annot")
     go_bp_annot <- read.delim(gba_file,
                               sep = "\t",
-                              header = TRUE,
+                              header = FALSE,
                               stringsAsFactors = FALSE,
                               comment.char = "!")
     go_bp_annot <- go_bp_annot[,1:2]
     colnames(go_bp_annot) <- c("gene","function")
-    go_bp_annots[[spe]] <- go_bp_annot
+    bpfile <- paste0(edpath, "annot_GO_", spe, ".rda")
+    save(go_bp_annot, file = bpfile, compress = "bzip2")
 }
-devtools::use_data(go_bp_annots, pkg = ".")
+
 
 # Uni_bp_annots
-uni_bp_annots <- NULL
 for(spe in species){
     gba_folder <- paste0("private/annotations/", spe, "/")
     uba_file <- paste0(gba_folder, "/uniprot_keywords_",
                        spe, "__biological_process.annot")
     uni_bp_annot <- read.delim(uba_file,
                               sep = "\t",
-                              header = TRUE,
+                              header = FALSE,
                               stringsAsFactors = FALSE,
                               comment.char = "!")
     uni_bp_annot <- uni_bp_annot[,1:2]
     colnames(uni_bp_annot) <- c("gene","function")
-    uni_bp_annots[[spe]] <- uni_bp_annot
+    ubpfile <- paste0(edpath, "annot_uniprot_", spe, ".rda")
+    save(uni_bp_annot, file = ubpfile, compress = "bzip2")
 }
-devtools::use_data(uni_bp_annots, pkg = ".")
 
 # Entrez_HGNC
-entrez_hgnc <- NULL
 for(spe in species){
     eh_folder <- paste0("private/annotations/", spe, "/")
     eh_file <- paste0(eh_folder, "/entrez_hgnc_", spe, ".annot")
-    eh_annot <- read.delim(eh_file,
-                           sep = "\t",
-                           header = FALSE,
-                           stringsAsFactors = FALSE,
-                           comment.char = "!")
-    entrez_hgnc[[spe]] <- eh_annot
+    entrez_hgnc <- read.delim(eh_file,
+                              sep = "\t",
+                              header = FALSE,
+                              stringsAsFactors = FALSE,
+                              comment.char = "!")
+    ehfile <- paste0(edpath, "entrez_hgnc_", spe, ".rda")
+    save(entrez_hgnc, file = ehfile, compress = "bzip2")
 }
-devtools::use_data(entrez_hgnc, pkg = ".")
 
 
 
 # PATHWAYS
 #------------------------
-# Separatedly
-# HSA
-mgi <- load("private/pathways/hsa/temp/meta_graph_info_hsa.RData")
-meta_graph_info_hsa <- get(mgi)
-meta_graph_info_hsa$species <- "hsa"
-devtools::use_data(meta_graph_info_hsa, pkg = ".")
-rm(metaginfo, meta_graph_info_hsa, mgi)
+# All together
+for(spe in species){
+    path <- paste0("private/pathways/", spe, "/temp/meta_graph_info_",
+                   spe, ".RData")
+    mgi <- load(path)
+    meta_graph_info <- get(mgi)
+    meta_graph_info$species <- spe
+    mgifile <- paste0(edpath, "meta_graph_info_", spe, ".rda")
+    save(meta_graph_info, file = mgifile, compress = "bzip2")
+}
 
-# RNO
-mgi <- load("private/pathways/rno/temp/meta_graph_info_rno.RData")
-meta_graph_info_rno <- get(mgi)
-meta_graph_info_rno$species <- "rno"
-devtools::use_data(meta_graph_info_rno, pkg = ".")
-rm(metaginfo, meta_graph_info_rno, mgi)
 
-# MMU
-mgi <- load("private/pathways/mmu/temp/meta_graph_info_mmu.RData")
-meta_graph_info_mmu <- get(mgi)
-meta_graph_info_mmu$species <- "mmu"
-devtools::use_data(meta_graph_info_mmu, pkg = ".")
-rm(metaginfo, meta_graph_info_mmu, mgi)
+# # Separatedly
+# # HSA
+# mgi <- load("private/pathways/hsa/temp/meta_graph_info_hsa.RData")
+# meta_graph_info_hsa <- get(mgi)
+# meta_graph_info_hsa$species <- "hsa"
+# save(meta_graph_info_hsa, pkg = ".")
+# rm(metaginfo, meta_graph_info_hsa, mgi)
+# 
+# # RNO
+# mgi <- load("private/pathways/rno/temp/meta_graph_info_rno.RData")
+# meta_graph_info_rno <- get(mgi)
+# meta_graph_info_rno$species <- "rno"
+# save(meta_graph_info_rno, pkg = ".")
+# rm(metaginfo, meta_graph_info_rno, mgi)
+# 
+# # MMU
+# mgi <- load("private/pathways/mmu/temp/meta_graph_info_mmu.RData")
+# meta_graph_info_mmu <- get(mgi)
+# meta_graph_info_mmu$species <- "mmu"
+# save(meta_graph_info_mmu, pkg = ".")
+# rm(metaginfo, meta_graph_info_mmu, mgi)
 
 
 
 
 # ANNOFUNS
 #------------------------
-# GO
-annofuns_GO <- NULL
-for(spe in species){
-    load(paste0("private/annofuns/annofuns_GO_", spe, ".RData"))
-    annofuns_GO[[spe]] <- annofuns
+for(db in dbs){
+    for(spe in species){
+        load(paste0("private/annofuns/annofuns_", db, "_", spe, ".RData"))
+        save(annofuns, file = paste0(edpath, "annofuns_", db, "_", spe, ".rda"), 
+             compress = "bzip2")
+    }
 }
-devtools::use_data(annofuns_GO, pkg = ".", internal = F)
-
-# Uniprot
-annofuns_uniprot <- NULL
-for(spe in species){
-    load(paste0("private/annofuns/annofuns_uniprot_", spe, ".RData"))
-    annofuns_uniprot[[spe]] <- annofuns
-}
-devtools::use_data(annofuns_uniprot, pkg = ".", internal = F)
 
 
 
 # PSEUDO PATHWAYS
 #------------------------
-# HSA
-pmgi <- load("private/pathways/pseudo/pmgi_hsa_genes.RData")
-pmgi_hsa_genes <- get(pmgi)
-devtools::use_data(pmgi_hsa_genes, pkg = ".")
-rm(pmgi_hsa_genes, pmgi)
+feats <- c("genes", "uniprot", "GO")
+for(spe in species){
+    for(feat in feats){
+        file <- paste0("private/pathways/pseudo/pmgi_", spe, "_", feat,".RData")
+        pseudo <- load(file)
+        pmgi <- get(pseudo)
+        save(pmgi, file = paste0(edpath, "pmgi_", spe, "_", feat, ".rda"), 
+             compress = "bzip2")
+        rm(list = pseudo)
+    }
+}
 
-pmgi <- load("private/pathways/pseudo/pmgi_hsa_uniprot.RData")
-pmgi_hsa_uniprot <- get(pmgi)
-devtools::use_data(pmgi_hsa_uniprot, pkg = ".")
-rm(pmgi_hsa_uniprot, pmgi)
-
-pmgi <- load("private/pathways/pseudo/pmgi_hsa_GO.RData")
-pmgi_hsa_GO <- get(pmgi)
-devtools::use_data(pmgi_hsa_GO, pkg = ".")
-rm(pmgi_hsa_GO, pmgi)
-
-# RNO
-pmgi <- load("private/pathways/pseudo/pmgi_rno_genes.RData")
-pmgi_rno_genes <- get(pmgi)
-devtools::use_data(pmgi_rno_genes, pkg = ".")
-rm(pmgi_rno_genes, pmgi)
-
-pmgi <- load("private/pathways/pseudo/pmgi_rno_uniprot.RData")
-pmgi_rno_uniprot <- get(pmgi)
-devtools::use_data(pmgi_rno_uniprot, pkg = ".")
-rm(pmgi_rno_uniprot, pmgi)
-
-pmgi <- load("private/pathways/pseudo/pmgi_rno_GO.RData")
-pmgi_rno_GO <- get(pmgi)
-devtools::use_data(pmgi_rno_GO, pkg = ".")
-rm(pmgi_rno_GO, pmgi)
-
-# MMU
-pmgi <- load("private/pathways/pseudo/pmgi_mmu_genes.RData")
-pmgi_mmu_genes <- get(pmgi)
-devtools::use_data(pmgi_mmu_genes, pkg = ".")
-rm(pmgi_mmu_genes, pmgi)
-
-pmgi <- load("private/pathways/pseudo/pmgi_mmu_uniprot.RData")
-pmgi_mmu_uniprot <- get(pmgi)
-devtools::use_data(pmgi_mmu_uniprot, pkg = ".")
-rm(pmgi_mmu_uniprot, pmgi)
-
-pmgi <- load("private/pathways/pseudo/pmgi_mmu_GO.RData")
-pmgi_mmu_GO <- get(pmgi)
-devtools::use_data(pmgi_mmu_GO, pkg = ".")
-rm(pmgi_mmu_GO, pmgi)
-
-
+# # HSA
+# pmgi <- load("private/pathways/pseudo/pmgi_hsa_genes.RData")
+# pmgi_hsa_genes <- get(pmgi)
+# save(pmgi_hsa_genes, pkg = ".")
+# rm(pmgi_hsa_genes, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_hsa_uniprot.RData")
+# pmgi_hsa_uniprot <- get(pmgi)
+# save(pmgi_hsa_uniprot, pkg = ".")
+# rm(pmgi_hsa_uniprot, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_hsa_GO.RData")
+# pmgi_hsa_GO <- get(pmgi)
+# save(pmgi_hsa_GO, pkg = ".")
+# rm(pmgi_hsa_GO, pmgi)
+# 
+# # RNO
+# pmgi <- load("private/pathways/pseudo/pmgi_rno_genes.RData")
+# pmgi_rno_genes <- get(pmgi)
+# save(pmgi_rno_genes, pkg = ".")
+# rm(pmgi_rno_genes, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_rno_uniprot.RData")
+# pmgi_rno_uniprot <- get(pmgi)
+# save(pmgi_rno_uniprot, pkg = ".")
+# rm(pmgi_rno_uniprot, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_rno_GO.RData")
+# pmgi_rno_GO <- get(pmgi)
+# save(pmgi_rno_GO, pkg = ".")
+# rm(pmgi_rno_GO, pmgi)
+# 
+# # MMU
+# pmgi <- load("private/pathways/pseudo/pmgi_mmu_genes.RData")
+# pmgi_mmu_genes <- get(pmgi)
+# save(pmgi_mmu_genes, pkg = ".")
+# rm(pmgi_mmu_genes, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_mmu_uniprot.RData")
+# pmgi_mmu_uniprot <- get(pmgi)
+# save(pmgi_mmu_uniprot, pkg = ".")
+# rm(pmgi_mmu_uniprot, pmgi)
+# 
+# pmgi <- load("private/pathways/pseudo/pmgi_mmu_GO.RData")
+# pmgi_mmu_GO <- get(pmgi)
+# save(pmgi_mmu_GO, pkg = ".")
+# rm(pmgi_mmu_GO, pmgi)
+# 
+# 
